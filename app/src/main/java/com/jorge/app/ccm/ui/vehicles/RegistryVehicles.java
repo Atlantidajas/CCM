4dfcad4f9aff5ab3ac586a3fcf68a656029294b2
Jorge H <jorge.hlt@hotmail.com>
2020-03-28 19:18:43 +0000

    add spinnerBrands

--- app/src/main/java/com/jorge/app/ccm/ui/vehicles/RegistryVehicles.java
+++ app/src/main/java/com/jorge/app/ccm/ui/vehicles/RegistryVehicles.java
@@ -6,10 +6,8 @@ import androidx.fragment.app.DialogFragment;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.TextView;
-import android.widget.ToggleButton;
+import android.widget.EditText;
 
 import com.jorge.app.ccm.R;
 import com.jorge.app.ccm.ui.alertsDialogos.DialogFragmentSelect;
@@ -18,44 +16,43 @@ import com.jorge.app.ccm.ui.form.FormRegistryBrands;
 public class RegistryVehicles extends AppCompatActivity implements DialogFragmentSelect.DialogFragmentListener {
 
     private FormRegistryBrands formRegistryBrands;
-    private Button buttonBrand;
+    private EditText buttonBrand;
+    private CheckBox checkBoxConfirmBrand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registry_vehicles);
-        buttonBrand = findViewById( R.id.button_brand_registry_vehicle );
+        buttonBrand = findViewById( R.id.edit_text_brand_registry_vehicle);
+        checkBoxConfirmBrand = findViewById( R.id.checkBox_brand_registry_vehicle );
+        checkBoxConfirmBrand.isChecked();
 
         buttonBrand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getFormRegistryBrands();
+
             }
         });
 
     }
 
-    public FormRegistryBrands getFormRegistryBrands(){
+    public void getFormRegistryBrands(){
         Resources res = getResources();
         String[] manufactures = res.getStringArray(R.array.manufactures);
         this.formRegistryBrands = new FormRegistryBrands( getSupportFragmentManager(), manufactures );
-        return formRegistryBrands;
     }
 
     //Onclik sobre item de FormRegistryBrands
     @Override
     public void onDialogItemClick(DialogFragment dialog) {
-
-        //System.out.println( "Item pulsado: " + this.formRegistryBrands.getItemResult() );
-        System.out.println("************************************************************************************************");
-        System.out.println( "Texto pulsado: " + this.formRegistryBrands.textItem(this.formRegistryBrands.getItemResult() ) );
-        System.out.println("************************************************************************************************");
-
+        String resultItemSelect = formRegistryBrands.textItem(formRegistryBrands.getItemResult() );
+        buttonBrand.setText( resultItemSelect );
+        checkBoxConfirmBrand.setChecked(true);
     }
 
     @Override
     public void onDialogFragmentSelectPositiveClick(DialogFragment dialog) {
-        //System.out.println("Pulsado siguiente >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
     }
 
     @Override
