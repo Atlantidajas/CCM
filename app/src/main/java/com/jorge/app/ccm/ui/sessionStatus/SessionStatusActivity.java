7f14725585ae0a565965acd186ef7da0c3a87e40
Jorge H <jorge.hlt@hotmail.com>
2020-05-18 11:03:22 +0100

    Add sessionStatus

--- app/src/main/java/com/jorge/app/ccm/ui/sessionStatus/SessionStatusActivity.java
+++ app/src/main/java/com/jorge/app/ccm/ui/sessionStatus/SessionStatusActivity.java
@@ -30,6 +30,9 @@ import com.jorge.app.ccm.models.User;
 import com.jorge.app.ccm.ui.sessionHistoric.SessionHistoricActivity;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import static com.jorge.app.ccm.ui.sessionHistoric.SessionHistoricActivity.SESSION_HISTORIC;
 
 public class SessionStatusActivity extends AppCompatActivity {
@@ -84,55 +87,81 @@ public class SessionStatusActivity extends AppCompatActivity {
 
         intentCloseSesion  = new Intent( SessionStatusActivity.this, SessionHistoricActivity.class );
 
-        Session sessionIntent = (Session) getIntent().getExtras().getSerializable( SESSION_HISTORIC );//<- El Inten
-        System.out.println( sessionIntent.getHours() + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" );
+        final Session sessionIntent = (Session) getIntent().getExtras().getSerializable( SESSION_HISTORIC );//<- El Inten
+        final ArrayList<SessionDriving>listSessionDriving = new ArrayList<>(  );
 
         // Cargo array adapte
-        this.controllerDBSessionsHistoric.getDatabaseReference().child( "session" ).addValueEventListener( new ValueEventListener() {
+        this.controllerDBSessionsHistoric.getDatabaseReference().addValueEventListener( new ValueEventListener() {
 
             @Override
             public void onDataChange( DataSnapshot dataSnapshot ) {
 
                 if (dataSnapshot.exists()) {
 
-                    Session session = new Session( dataSnapshot );
-                    sessionDriving = new SessionDriving( dataSnapshot );
+                    Iterator<DataSnapshot> dataSnapshots = dataSnapshot.getChildren().iterator();
 
-                    Resources resources = getResources();
-                    String typeSessionStart = resources.getString( R.string.textViewTypeSessionStart );
-                    String typeSessionEnd = resources.getString( R.string.textViewTypeSessionEnd );
-                    String userSession = resources.getString( R.string.textViewUserDate );
-                    String dateSession = resources.getString( R.string.textViewDate );
-                    String hoursSession = resources.getString( R.string.textViewHours );
-                    String brandVehicleSession = resources.getString( R.string.textViewBrand );
-                    String modelVehicleSession = resources.getString( R.string.textViewModel );
-                    String registrationNumberVehicleSession = resources.getString( R.string.textViewRegistrationNumber );
+                    do{
+                        SessionDriving sessionDriving = new SessionDriving( dataSnapshots.next() );
 
-                    Glide.with( getApplicationContext() ).load( sessionDriving.getUser().getPhotoUriString() ).into( imageViewUserDate );
-                    switch ( sessionDriving.getSession().getTypeSesion() ){
-                        case "Start":
-                            imageViewTypeSession.setImageResource( R.mipmap.ic_launcher_open_lock );
-                            textViewTypeSession.setText( typeSessionStart );
-                            break;
-                        case "End":
-                            imageViewTypeSession.setImageResource( R.mipmap.ic_launcher_close_lock );
-                            textViewTypeSession.setText( typeSessionEnd );
-                            break;
-                        default:
-                            imageViewTypeSession.setImageResource( R.mipmap.ic_launcher_open_lock );
-                            textViewTypeSession.setText( sessionDriving.getSession().getTypeSesion() );
-                            break;
+                        if( sessionDriving.getUser().getIdUser().equals( user.getIdUser() ) ) {
+
+                            //Listado de todas las sessiones históricas de la db
+                            listSessionDriving.add( sessionDriving );
+
+                        }
+
+
+                    }while (dataSnapshots.hasNext());
+
+                    //Recorro todas las sesiones históricas de la db
+                    for( int i = 0; i < listSessionDriving.size(); i++ ){
+
+                        //Solo me quedo con las sesiones del usuario y con la que tenga el mismo registro de tiempo que la recibida por el intent
+                        if( listSessionDriving.get( i ).getUser().getIdUser().equals( user.getIdUser() )  &&
+                                ( listSessionDriving.get( i ).getSession().getHours().equals( sessionIntent.getHours() ) )  ){
+
+                            sessionDriving = listSessionDriving.get( i );
+
+                            Log.i( TAG, sessionDriving.getUser().getIdUser() );
+
+                            Resources resources = getResources();
+                            String typeSessionStart = resources.getString( R.string.textViewTypeSessionStart );
+                            String typeSessionEnd = resources.getString( R.string.textViewTypeSessionEnd );
+                            String userSession = resources.getString( R.string.textViewUserDate );
+                            String dateSession = resources.getString( R.string.textViewDate );
+                            String hoursSession = resources.getString( R.string.textViewHours );
+                            String brandVehicleSession = resources.getString( R.string.textViewBrand );
+                            String modelVehicleSession = resources.getString( R.string.textViewModel );
+                            String registrationNumberVehicleSession = resources.getString( R.string.textViewRegistrationNumber );
+
+                            Glide.with( getApplicationContext() ).load( sessionDriving.getUser().getPhotoUriString() ).into( imageViewUserDate );
+                            switch ( sessionDriving.getSession().getTypeSesion() ){
+                                case "Start":
+                                    imageViewTypeSession.setImageResource( R.mipmap.ic_launcher_open_lock );
+                                    textViewTypeSession.setText( typeSessionStart );
+                                    break;
+                                case "End":
+                                    imageViewTypeSession.setImageResource( R.mipmap.ic_launcher_close_lock );
+                                    textViewTypeSession.setText( typeSessionEnd );
+                                    break;
+                                default:
+                                    imageViewTypeSession.setImageResource( R.mipmap.ic_launcher_open_lock );
+                                    textViewTypeSession.setText( sessionDriving.getSession().getTypeSesion() );
+                                    break;
+                            }
+                            textViewUserDate.setText( userSession + " " + sessionDriving.getUser().getName() );
+                            textViewDate.setText( dateSession + " " + sessionDriving.getSession().getDate() );
+                            textViewHours.setText( hoursSession  + " " + sessionDriving.getSession().getHours() );
+                            textViewBrand.setText( brandVehicleSession +  " " + sessionDriving.getVehicle().getBrand() );
+                            imageViewBrand.setImageResource( sessionDriving.getVehicle().getLogoVehicle() );
+                            textViewModel.setText( modelVehicleSession + " " + sessionDriving.getVehicle().getModel() );
+                            textViewRegistrationNumber.setText( registrationNumberVehicleSession + " " + sessionDriving.getVehicle().getRegistrationNumber() );
+
+                        }
                     }
-                    textViewUserDate.setText( userSession + " " + sessionDriving.getUser().getName() );
-                    textViewDate.setText( dateSession + " " + sessionDriving.getSession().getDate() );
-                    textViewHours.setText( hoursSession  + " " + sessionDriving.getSession().getHours() );
-                    textViewBrand.setText( brandVehicleSession +  " " + sessionDriving.getVehicle().getBrand() );
-                    imageViewBrand.setImageResource( sessionDriving.getVehicle().getLogoVehicle() );
-                    textViewModel.setText( modelVehicleSession + " " + sessionDriving.getVehicle().getModel() );
-                    textViewRegistrationNumber.setText( registrationNumberVehicleSession + " " + sessionDriving.getVehicle().getRegistrationNumber() );
-
                 }
                 else {
+                    //En caso no tener ningún registro, informo al usuario
                     Toast.makeText( getApplicationContext(), R.string.toast_message_no_data, Toast.LENGTH_SHORT).show();
                 }
             }
