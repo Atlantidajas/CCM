d477c7ea506d45fdd5c9f57830e9759f7e887836
Jorge H <jorge.hlt@hotmail.com>
2020-06-07 19:08:01 +0100

    add mapbox

--- app/src/main/java/com/jorge/app/ccm/ui/location/LocationActivity.java
+++ app/src/main/java/com/jorge/app/ccm/ui/location/LocationActivity.java
@@ -1,16 +1,140 @@
 package com.jorge.app.ccm.ui.location;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
 import com.jorge.app.ccm.R;
 
 public class LocationActivity extends AppCompatActivity {
 
+    private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_location );
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString( R.string.tokenMapBox ));
+        setContentView(R.layout.activity_location);
+        mapView = findViewById( R.id.mapView );
+        mapView.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//<-- añado flecha retroseso
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+
+                String name = "Santa Cruz de Tenerife";
+                Double latitude = 28.4636296;
+                Double loyalty = -16.2518467;
+
+                CameraPosition position = new CameraPosition.Builder()
+                        .target(new LatLng(latitude, loyalty)) // Fija la posición
+                        .zoom(17) // Fija el nivel de zoom
+                        .tilt(30) // Fija la inclinación de la cámara
+                        .build();
+
+                mapboxMap.animateCamera( CameraUpdateFactory
+                        .newCameraPosition(position), 7000);
+
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position( position.target )
+                        .title( name )
+                        .snippet("Latitud: " + latitude.toString() + " Longitud: " + loyalty.toString()) );
+
+
+
+                messageToast( name );
+
+
+                //mapboxMap.getCameraPosition();
+
+                //onStart();
+
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                    }
+                });
+            }
+
+
+        });
+
+
+        onStart();
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if ( id == android.R.id.home ){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);//<-- Devuelve una opción de menú la pulsada (Método de la clase padre).
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    // Muestra una tostada.
+    private void messageToast(String mensaje) {
+        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT).show();
+    }
+
 }
